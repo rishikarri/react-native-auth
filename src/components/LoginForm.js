8205ce148dc3c06d753bf57b0b752861be849030
Rishi Karri <rishikarri1@gmail.com>
2017-04-12 10:44:13 -0400

    login form changes

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { Button, Card, CardSection, Input } from './common/index.js';
+import firebase from 'firebase';
 
 class LoginForm extends Component {
 	state = { email: '', password: '' };
 
+	onButtonPress(){
+		const { email, password } = this.state;
+		firebase.auth().signInWithEmailAndPassword( email, password );
+	}
 
 	render() {
 		return (
@@ -32,7 +37,7 @@ class LoginForm extends Component {
 
 
 				<CardSection >
-					<Button> Login </Button>
+					<Button onPress={this.onButtonPress.bind(this)}> Login </Button>
 				</CardSection >
 			</Card>
 		);
