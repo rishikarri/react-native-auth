4558a40f473ef5848759e64b3f7076d1a60622f4
Rishi Karri <rishikarri1@gmail.com>
2017-04-12 23:21:26 -0400

    spinner complete

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View } from 'react-native' 
+import { View, ActivityIndicator } from 'react-native';
+
 
 // presentational component - no need for state 
 
-const Spinner = () => {
+const Spinner = ({ sizeOfSpinner }) => {
 	return ( 
-		<View />		
-	)
-}
+		<View style={styles.spinnerStyle}>
+			<ActivityIndicator size={sizeOfSpinner || 'large'}/>
+		</View>
+
+		
+	);
+};
 
 export default { Spinner };
+
+const styles = {
+	spinnerStyle: {
+		flex: 1, 
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+}
 
